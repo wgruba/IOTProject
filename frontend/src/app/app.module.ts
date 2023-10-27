c34294798bf477f8104acc4deb9d930ade879bd2
wgruba <wgruba08@gmail.com>
2023-10-27 18:49:02 +0200

    done header and main site started

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,33 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA,NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { register } from 'swiper/element/bundle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainSiteComponent } from './main-site/main-site.component';
 import { UserHeaderComponent } from './user-header/user-header.component';
 import { UserFooterComponent } from './user-footer/user-footer.component';
+import { SwiperDirective } from './swiper.directive';
+
+
+register();
 
 @NgModule({
   declarations: [
     AppComponent,
     MainSiteComponent,
     UserHeaderComponent,
-    UserFooterComponent
+    UserFooterComponent,
+    SwiperDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
