2af9a50c76bbff1c3d9c58bbbaa74eb673654ef7
Rysiek Polkowski <101052665+Drysiek@users.noreply.github.com>
2023-10-27 18:01:49 +0200

    category class added
    User, Event and Category classes complete

--- backend- spring/spring-boot/src/main/java/com/example/springboot/Category/Category.java
+++ backend- spring/spring-boot/src/main/java/com/example/springboot/Category/Category.java
@@ -1,8 +1,62 @@
 package com.example.springboot.Category;
 
+
+import java.util.List;
 public class Category {
     private int id;
+    private String name;
+    private boolean isParentCategory;
+    private List<Integer> subcategories;
+    private int parentId;
+
+    public Category() {
+    }
+
+    public Category(int id, String name, boolean isParentCategory, List<Integer> subcategories, int parentId) {
+        this.id = id;
+        this.name = name;
+        this.isParentCategory = isParentCategory;
+        this.subcategories = subcategories;
+        this.parentId = parentId;
+    }
+
     public int getId() {
-    return id;
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isParentCategory() {
+        return isParentCategory;
+    }
+
+    public void setParentCategory(boolean parentCategory) {
+        isParentCategory = parentCategory;
+    }
+
+    public List<Integer> getSubcategories() {
+        return subcategories;
+    }
+
+    public void setSubcategories(List<Integer> subcategories) {
+        this.subcategories = subcategories;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
     }
 }
