f70364be942f5eb5c6a30a0a9f31339d96a0d59e
wgruba <wgruba08@gmail.com>
2023-11-23 14:57:55 +0100

    fix annotations

--- backend- spring/spring-boot/src/main/java/com/example/springboot/Category/CategoryController.java
+++ backend- spring/spring-boot/src/main/java/com/example/springboot/Category/CategoryController.java
@@ -8,13 +8,13 @@ import com.example.springboot.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "http://localhost:4200/")
+@RestController
 public class CategoryController {
 
     @Autowired
--- backend- spring/spring-boot/src/main/java/com/example/springboot/User/UserController.java
+++ backend- spring/spring-boot/src/main/java/com/example/springboot/User/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 public class UserController {
 
