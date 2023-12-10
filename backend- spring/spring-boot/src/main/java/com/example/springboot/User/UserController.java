ec5c1b81585f8f1a05bcf40a68f064d71f5710bf
Rysiek Polkowski <101052665+Drysiek@users.noreply.github.com>
2023-12-10 15:53:16 +0100

    edycja wydarzeń przechodzą przez moderatorów

--- backend- spring/spring-boot/src/main/java/com/example/springboot/User/UserController.java
+++ backend- spring/spring-boot/src/main/java/com/example/springboot/User/UserController.java
@@ -4,6 +4,7 @@ import com.example.springboot.Category.Category;
 import com.example.springboot.Category.CategoryController;
 import com.example.springboot.Event.Event;
 import com.example.springboot.Event.EventController;
+import com.example.springboot.Event.EventStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -65,21 +66,21 @@ public class UserController {
     {
         List<User> users = userRepository.findAll();
         List<User> filteredUsers = new ArrayList<>();
-        for(User user: users){
+        for(User user: users) {
             int tot = 0;
-            if(user.getName().toLowerCase().contains(name.toLowerCase()))
+            if (user.getName().toLowerCase().contains(name.toLowerCase()))
                 tot++;
-            if(user.getMail().toLowerCase().contains(mail.toLowerCase()))
+            if (user.getMail().toLowerCase().contains(mail.toLowerCase()))
                 tot++;
-            if(user.getPermissionLevel() == PermissionLevel.ADMIN && admin)
+            if (user.getPermissionLevel() == PermissionLevel.ADMIN && admin)
                 tot++;
-            if(user.getPermissionLevel() == PermissionLevel.MODERATOR && mod)
+            if (user.getPermissionLevel() == PermissionLevel.MODERATOR && mod)
                 tot++;
-            if(user.getPermissionLevel() == PermissionLevel.VERIFIED_USER && ver)
+            if (user.getPermissionLevel() == PermissionLevel.VERIFIED_USER && ver)
                 tot++;
-            if(user.getPermissionLevel() == PermissionLevel.UNVERIFIED_USER && nonver)
+            if (user.getPermissionLevel() == PermissionLevel.UNVERIFIED_USER && nonver)
                 tot++;
-            if(tot == 3)
+            if (tot == 3)
                 filteredUsers.add(user);
         }
         List<UserDTO> userDTOs = filteredUsers.stream()
@@ -88,7 +89,7 @@ public class UserController {
         return ResponseEntity.ok(userDTOs);
     }
     @GetMapping("/users/{id}")
-    public ResponseEntity<UserDTO> getUser(@PathVariable int id){
+    public ResponseEntity<UserDTO> getUser(@PathVariable int id) {
         Optional<User> existingUser = userRepository.getUserById(id);
         if (existingUser.isPresent()) {
             User user = existingUser.get();
@@ -178,25 +179,25 @@ public class UserController {
 
     // Account management
     @GetMapping("users/login")
-    public ResponseEntity<Boolean> tryToLoginUser(String loginOrMail, String password){
+    public ResponseEntity<Boolean> tryToLoginUser(String loginOrMail, String password) {
         boolean result = userRepository.login(loginOrMail, password).isPresent();
         return ResponseEntity.ok(result);
     }
     @GetMapping("/users/{userId}/getPermissionLevel")
-    public ResponseEntity<PermissionLevel> getPermissionLevel(@PathVariable int userId){
+    public ResponseEntity<PermissionLevel> getPermissionLevel(@PathVariable int userId) {
         Optional<User> existingUser = userRepository.getUserById(userId);
         return existingUser.map(user -> ResponseEntity.ok(user.getPermissionLevel()))
                 .orElseGet(() -> ResponseEntity.ok(PermissionLevel.UNVERIFIED_USER));
     }
     @PutMapping("/users/{userId}/changePermissions")
-    public ResponseEntity<Boolean> changePermissionLevel(@PathVariable int userId, @RequestBody PermissionLevel permissionLevel){
+    public ResponseEntity<Boolean> changePermissionLevel(@PathVariable int userId, @RequestBody PermissionLevel permissionLevel) {
         userRepository.findById(userId).map(user -> {
                     user.setPermissionLevel(permissionLevel);
         return ResponseEntity.ok(true);});
         return ResponseEntity.ok(false);
     }
     @GetMapping("/users/{userId}/getName")
-    public ResponseEntity<String> getUsersName(@PathVariable int userId){
+    public ResponseEntity<String> getUsersName(@PathVariable int userId) {
         Optional<User> existingUser = userRepository.getUserById(userId);
         if (existingUser.isPresent()) {
             User tempUser = existingUser.get();
@@ -231,7 +232,7 @@ public class UserController {
         if (existingUser.isPresent()) {
             User tempUser = existingUser.get();
             List<Integer> tempList = tempUser.getSubscribedEvents();
-            if(tempList.contains(eventId))
+            if (tempList.contains(eventId))
                 return ResponseEntity.ok(false);
             tempList.add(eventId);
             tempUser.setSubscribedEvents(tempList);
@@ -247,7 +248,7 @@ public class UserController {
         if (existingUser.isPresent()) {
             User tempUser = existingUser.get();
             List<Integer> tempList = tempUser.getSubscribedCategories();
-            if(tempList.contains(categoryId))
+            if (tempList.contains(categoryId))
                 return ResponseEntity.ok(false);
             tempList.add(categoryId);
             tempUser.setSubscribedCategories(tempList);
@@ -264,7 +265,7 @@ public class UserController {
             List<Integer> tempList = tempUser.getSubscribedCategories();
 
             for(Integer categoryId: categoryList)
-                if(!tempList.contains(categoryId))
+                if (!tempList.contains(categoryId))
                     tempList.add(categoryId);
 
             tempUser.setSubscribedCategories(tempList);
@@ -278,11 +279,11 @@ public class UserController {
     public ResponseEntity<Boolean> unsubscribeEvent(@PathVariable int userId, @PathVariable Integer eventId) {
         Optional<User> existingUser = userRepository.getUserById(userId);
         if (existingUser.isPresent()) {
-            if(eventController.getEventOrganiser(eventId) == userId)
+            if (eventController.getEventOrganiser(eventId) == userId)
                 return ResponseEntity.ok(false);
             User tempUser = existingUser.get();
             List<Integer> tempList = tempUser.getSubscribedEvents();
-            if(!tempList.contains(eventId))
+            if (!tempList.contains(eventId))
                 return ResponseEntity.ok(false);
             tempList.remove(eventId);
             tempUser.setSubscribedEvents(tempList);
@@ -298,7 +299,7 @@ public class UserController {
         if (existingUser.isPresent()) {
             User tempUser = existingUser.get();
             List<Integer> tempList = tempUser.getSubscribedCategories();
-            if(!tempList.contains(categoryId))
+            if (!tempList.contains(categoryId))
                 return ResponseEntity.ok(false);
             tempList.remove(categoryId);
             tempUser.setSubscribedCategories(tempList);
@@ -311,17 +312,17 @@ public class UserController {
 
     // User's Events
     @PostMapping("/users/{userId}/createEvent")
-    public ResponseEntity<Event> createEvent(@PathVariable int userId, @RequestBody Event event){
+    public ResponseEntity<Event> createEvent(@PathVariable int userId, @RequestBody Event event) {
         ResponseEntity<Event> tempEvent = eventController.addEvent(event);
         subscribeEvent(userId, event.getId());
         return tempEvent;
     }
     @PatchMapping("/users/{userId}/deleteEvent/{eventId}")
-    public ResponseEntity<Boolean> deleteEvent(@PathVariable int userId, @PathVariable Integer eventId){
-        if(eventController.getEventOrganiser(eventId) != userId)
+    public ResponseEntity<Boolean> deleteEvent(@PathVariable int userId, @PathVariable Integer eventId) {
+        if (eventController.getEventOrganiser(eventId) != userId)
             return ResponseEntity.ok(false);
         List<Integer> userList = eventController.getUsersThatSubscribedToEvent(eventId);
-        for(Integer user:userList){
+        for(Integer user:userList) {
             unsubscribeEvent(user, eventId);
         }
 
@@ -341,34 +342,68 @@ public class UserController {
 
     }
     @GetMapping("users/{userId}/myEvents")
-    public ResponseEntity<List<Event>> getEventsOrganisedByUser(@PathVariable int userId){
+    public ResponseEntity<List<Event>> getEventsOrganisedByUser(@PathVariable int userId) {
         return ResponseEntity.ok(eventController.getEventsByOrganiser(userId));
     }
     @PatchMapping("/users/{userId}/updateEvent")
-    public ResponseEntity<?> updateEvent(@PathVariable int userId, @RequestBody Event event){
-        if(eventController.getEventOrganiser(event.getId()) != userId)
+    public ResponseEntity<?> updateEvent(@PathVariable int userId, @RequestBody Event event) {
+        if (eventController.getEventOrganiser(event.getId()) != userId)
             return ResponseEntity.ok(false);
-        return ResponseEntity.ok(eventController.updateEvent(event.getId(), event));
+        Optional<User> existingUser = userRepository.getUserById(userId);
+        if (existingUser.isPresent()) {
+            switch (existingUser.get().getPermissionLevel()){
+                case UNVERIFIED_USER:
+                    event.setId(-event.getId());
+                    event.setEventStatus(EventStatus.TO_ACCEPTANCE);
+                    createEvent(0, event);
+                    break;
+                default:
+                    return ResponseEntity.ok(eventController.updateEvent(event.getId(), event));
+            }
+        }
+        return ResponseEntity.ok(false);
     }
 
 
     // Moderator options
     @PostMapping("/users/{userId}/createCategory")
-    public ResponseEntity<?> createCategory(@PathVariable int userId, @RequestBody Category category){
+    public ResponseEntity<?> createCategory(@PathVariable int userId, @RequestBody Category category) {
         if (getPermissionLevel(userId).getBody() == PermissionLevel.MODERATOR || getPermissionLevel(userId).getBody() == PermissionLevel.ADMIN)
             return ResponseEntity.ok(categoryController.addCategory(category));
         return ResponseEntity.ok(null);
     }
     @PostMapping("/users/{userId}/createSubCategory")
-    public ResponseEntity<?> createSubCategory(@PathVariable int userId, @RequestBody Category subCategory){
+    public ResponseEntity<?> createSubCategory(@PathVariable int userId, @RequestBody Category subCategory) {
         if (getPermissionLevel(userId).getBody() == PermissionLevel.MODERATOR || getPermissionLevel(userId).getBody() == PermissionLevel.ADMIN)
             return ResponseEntity.ok(categoryController.addSubCategory(subCategory));
         return ResponseEntity.ok(null);
     }
     @PatchMapping("/users/{userId}/updateCategory")
-    public ResponseEntity<?> updateCategory(@PathVariable int userId, @RequestBody Category category){
+    public ResponseEntity<?> updateCategory(@PathVariable int userId, @RequestBody Category category) {
         if (getPermissionLevel(userId).getBody() == PermissionLevel.MODERATOR || getPermissionLevel(userId).getBody() == PermissionLevel.ADMIN)
             return ResponseEntity.ok(categoryController.updateCategory(category.getId(), category));
         return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/users/{userId}/acceptEvent/{eventId}")
+    public ResponseEntity<?> acceptEvent(@PathVariable int userId, @PathVariable Integer eventId) {
+        ResponseEntity<?> existingEvent = eventController.getEvent(eventId);
+        ResponseEntity<?> existingEvent2 = eventController.getEvent(-eventId);
+
+        if (existingEvent.getBody() instanceof Event && existingEvent2.getBody() instanceof Event) {
+            Event originalEvent = (Event) existingEvent.getBody();
+            Event copyEvent = (Event) existingEvent.getBody();
+
+            copyEvent.setOrganizer(originalEvent.getOrganizer());
+            copyEvent.setEventStatus(EventStatus.ACCEPTED);
+            copyEvent.setId(eventId);
+
+            eventController.deleteEvent(-eventId);
+            return ResponseEntity.ok(eventController.updateEvent(eventId, copyEvent));
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Cannot find event with given id");
     }
 }
