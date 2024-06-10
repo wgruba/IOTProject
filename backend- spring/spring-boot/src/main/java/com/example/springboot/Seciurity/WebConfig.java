68a9a964dab314545997d4cc0fcc2b346fadbb95
Batman <wgruba08@gmail.com>
2024-06-10 11:45:15 +0200

    add 2 users configuration

--- backend- spring/spring-boot/src/main/java/com/example/springboot/Seciurity/WebConfig.java
+++ backend- spring/spring-boot/src/main/java/com/example/springboot/Seciurity/WebConfig.java
@@ -14,10 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS" , "PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
